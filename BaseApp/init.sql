194649d0106dc9ab3a6efdc05b89e7b57a6e3212
Robbert Celen <robbert.celen@hotmail.be>
2022-12-16 16:09:01 +0100

    baseapp removed

--- BaseApp/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS examen;
-USE examen;
-CREATE TABLE IF NOT EXISTS todos (
-    id varchar(255) primary key,
-    title varchar(255) not null,
-    status varchar(255) not null
-);
-
-insert into todos (id, title, status) values (UUID(), 'mijn eerste taak', 'TODO');
